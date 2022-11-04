3ef601e02eff3114e20fe2544d98b62d182c3800
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2022-11-04 16:45:33 +0000

    Release 0.0.8

--- sample-app/src/main/java/sample/App.java
+++ sample-app/src/main/java/sample/App.java
@@ -2,34 +2,8 @@ package sample;
 
 import java.lang.String;
 
-import com.phyllo.connect.api.PhylloConnectApiClient;
-import com.phyllo.connect.api.client.connect.endpoints.CreateUser;
-import com.phyllo.connect.api.client.connect.exceptions.CreateUserException;
-import com.phyllo.connect.api.client.connect.types.CreateUserRequest;
-import com.phyllo.connect.api.client.connect.types.ExternalId;
-import com.phyllo.connect.api.client.connect.types.User;
-import com.phyllo.connect.api.core.BasicAuth;
-
 public final class App {
-    public static void main(String[] args) {
-        String client_id = System.getenv("PHYLLO_CLIENT_ID");
-        String secret = System.getenv("PHYLLO_SECRET");
-        BasicAuth auth = BasicAuth.of(client_id, secret);
-
-        PhylloConnectApiClient phylloConnectApiClient =
-                new PhylloConnectApiClient("api.getphyllo.com", auth);
-
-        try {
-            User user = phylloConnectApiClient.connect().createUser(CreateUser.Request.builder()
-                    .body(CreateUserRequest.builder()
-                            .name("John Doe")
-                            .externalId(ExternalId.valueOf("179a0bb4-c572-4477-9457-7dacf1b84848"))
-                            .build())
-                    .build());
-            System.out.println("Created a user! The user ID is " + user.getId());
-        } catch (CreateUserException e) {
-            System.out.println("Failed to create a user" + e.getMessage());
-        }
-
-    }
+  public static void main(String[] args) {
+    // import com.phyllo.connect.api.PhylloConnectApiClient
+  }
 }
